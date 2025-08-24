0f04c8f770fc6c636346d82ac33d64a267274561
pandatuu <395387944@qq.com>
2025-08-25 00:45:14 +0800

    添加UHFSDK的初始化和单次扫描、盘点的调用

--- app/src/main/java/com/example/uhfproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/uhfproject/ui/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.uhfproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.uhfproject.databinding.ActivityMainBinding
+import com.seuic.uhf.UHFService
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mViewModel: MainViewModel
+
+    private var uhfService: UHFService? = null
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(mBinding.root)
         mViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        uhfService = UHFService.getInstance()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // open UHF
+        if(uhfService == null){
+            uhfService = UHFService.getInstance()
+        }
+        val ret: Boolean = uhfService!!.open()
+        if (!ret) {
+            //UHF扫描加载失败
+
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        uhfService?.close()
     }
 }
--- app/src/main/java/com/example/uhfproject/ui/MainViewModel.kt
+++ app/src/main/java/com/example/uhfproject/ui/MainViewModel.kt
@@ -2,12 +2,82 @@ package com.example.uhfproject.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.uhfproject.app.MyApplication.Companion.appContext
 import com.example.uhfproject.room.AppDatabase
 import com.example.uhfproject.utils.RetrofitClient
+import com.seuic.uhf.EPC
+import com.seuic.uhf.UHFService
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
     private val rep = MainRep()
+
+
+    private val _epcList: MutableLiveData<List<EPC>> = MutableLiveData()
+    val epcList: LiveData<List<EPC>> = _epcList
+    /**
+     * 单次扫描
+     */
+    fun onceScan(){
+        val epc = EPC()
+        if(UHFService.getInstance().inventoryOnce(epc, 100)){
+            val id = epc.getId()
+            if (id != null && "" != id) {
+                val currentList = _epcList.value?.toMutableList() ?: mutableListOf()
+                if(currentList.all { it.getId() != id }){
+                    currentList.add(epc)
+                    _epcList.postValue(currentList)
+                }
+            }
+        }
+    }
+
+    private var mInventoryStart = false
+
+    /**
+     * 盘点监听
+     */
+    private var stockListener: Job? = null
+
+    fun startStock(){
+        if(UHFService.getInstance().inventoryStart()){
+            mInventoryStart = true
+            //开始盘点
+            if(stockListener == null){
+                stockListener =  viewModelScope.launch {
+                    while(mInventoryStart){
+                        val tagIds = UHFService.getInstance().tagIDs
+                        _epcList.postValue(tagIds)
+                        delay(100)
+                    }
+                }
+            }
+            stockListener!!.start()
+        } else {
+            //盘点失败
+        }
+    }
+
+    fun stopStock(){
+        if(UHFService.getInstance().inventoryStop()){
+            //停止盘点
+            stockListener?.let {
+                if(it.isActive){
+                    mInventoryStart = false
+                    it.cancel()
+                    stockListener = null
+                }
+            }
+        } else {
+            //停止失败
+        }
+    }
+
 }
 
 class MainRep(){
